24d6a469fad926a866b0cf5c431041a3efa56b7c
sevvalkk <sevvalkymzl@gmail.com>
2025-05-03 15:43:17 +0300

    The error has been fixed

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -11,13 +11,12 @@ import os
 
 KAFKA_BROKER = os.getenv('KAFKA_BROKER', 'kafka:9092')
 API_KEY = open("api_key.txt").read().strip()
-COUNTRIES = open("country_codes.txt").read().splitlines()
 
-snippet_features = ["title",
-                    "publishedAt",
-                    "channelId",
-                    "channelTitle",
-                    "categoryId"]
+features = ["title",
+            "publishedAt",
+            "channelId",
+            "channelTitle",
+            "categoryId"]
 
 def create_kafka_producer(retries=10, delay=5):
     for attempt in range(1, retries + 1):
@@ -47,8 +46,7 @@ def get_videos(items):
         snippet = video['snippet']
         statistics = video['statistics']
 
-        # Basic features from snippet
-        snippet_data = {feature: snippet.get(feature, "") for feature in snippet_features}
+        data = {feature: snippet.get(feature, "") for feature in features}
 
         description = snippet.get("description", "")
         thumbnail_link = snippet.get("thumbnails", {}).get("default", {}).get("url", "")
@@ -60,19 +58,15 @@ def get_videos(items):
         if 'likeCount' in statistics and 'dislikeCount' in statistics:
             likes = statistics['likeCount']
             dislikes = statistics['dislikeCount']
-            ratings_disabled = False
         else:
             likes = 0
             dislikes = 0
-            ratings_disabled = True
 
         # Comments
         if 'commentCount' in statistics:
             comment_count = statistics['commentCount']
-            comments_disabled = False
         else:
             comment_count = 0
-            comments_disabled = True
 
         video_data = {
             "video_id": video_id,
@@ -86,7 +80,7 @@ def get_videos(items):
             "description": description
         }
 
-        video_data.update(snippet_data)
+        video_data.update(data)
 
         videos.append(video_data)
 
@@ -102,8 +96,10 @@ import os
 
 def job():
     all_data = []  
+    with open("country_codes.txt") as f:
+        countries = f.read().splitlines() 
 
-    for country in COUNTRIES:
+    for country in countries:
         print(f"Fetching for {country}...")
         items = fetch_trending(country)
         print(f"Found {len(items)} items for {country}.")
